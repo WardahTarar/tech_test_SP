52808ff84d28c99cd510607a87b9330d1a3d34e9
Wardah <wlt_93@hotmail.com>
2019-05-31 20:48:15 +0100

    Display pages from most to least visited

--- lib/webserver.rb
+++ lib/webserver.rb
@@ -9,4 +9,10 @@ class WebServer
     @logs.push(log)
   end
 
+  def display_page_visits
+    @logs.each_with_object(Hash.new(0)) { |page, visit| 
+      visit[page] += 1 
+    }.sort
+  end
+
 end
--- spec/webserver_spec.rb
+++ spec/webserver_spec.rb
@@ -5,9 +5,17 @@ describe WebServer do
     webserver = WebServer.new
     expect(webserver.logs).to eq []
   end
-  
+
   it 'receives log as an arguement' do
     webserver = WebServer.new
     expect(webserver.receive_log('/parser')).to eq ['/parser']
+  end
+
+  it 'displays pages from most visited to least visited' do
+    webserver = WebServer.new
+    5.times { webserver.receive_log('/home')}
+    3.times { webserver.receive_log('/index')}
+    7.times { webserver.receive_log('/about')}
+    expect(webserver.display_page_visits()).to eq [["/about", 7], ["/home", 5], ["/index", 3]]
   end
 end
